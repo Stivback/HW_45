a708a2f4a0e218f0c2e89179a03ff7bdb1fff3aa
alexey <alexey@192.168.0.14>
2017-10-28 11:31:53 -0700

    my

--- src/main/java/core/ConfirmationPF.java
+++ src/main/java/core/ConfirmationPF.java
@@ -8,7 +8,21 @@ public class ConfirmationPF {
 		
 		CommonPF.open(url);
 		
-		CommonPF.pageValidationPF("03. Element [First Name (field)]: ", CommonPF.pf_el_07);
+		CommonPF.pageValidationPF("01. Element [Title]: ", CommonPF.pf_el_01);
+		CommonPF.pageValidationPF("02. Element [First Name (label)]: ", CommonPF.pf_el_02);
+		CommonPF.pageValidationPF("03. First Name (field)]: ", CommonPF.pf_el_03);
+		CommonPF.pageValidationPF("04. Last Name (label)]: ", CommonPF.pf_el_04);
+		CommonPF.pageValidationPF("05. Last Name (field)]: ", CommonPF.pf_el_05);
 		
-		}
+		
+		CommonPF.pageValidationPF("06. Element [Email (label]: ", CommonPF.pf_el_06);
+		CommonPF.pageValidationPF("07. Element [Email (filed)]: ", CommonPF.pf_el_07);
+		CommonPF.pageValidationPF("08. Element [Phone (label)]: ", CommonPF.pf_el_08);
+		CommonPF.pageValidationPF("09. Element [Phone (field)]: ", CommonPF.pf_el_09);
+		CommonPF.pageValidationPF("10. Element [Genre (label)]: ", CommonPF.pf_el_10);
+		
+		CommonPF.pageValidationPF("11. Element [Genre (field)]: ", CommonPF.pf_el_11);
+		CommonPF.pageValidationPF("12. Element [Back (button)]: ", CommonPF.pf_el_12);
+
+	}
 }
--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -8,10 +8,22 @@ public class Main {
 	static WebDriver driver;
 	public static void main(String[] args) {
 		System.out.println("Browser: HtmlUnit");
+		
+		
 		start = System.currentTimeMillis();
+		
+		
 		SignUpPF.validate(driver, "http://alex.academy/exe/signup/v1/index.php");
 		ConfirmationPF.validate(driver, "http://alex.academy/exe/signup/v1/confirmation.php");
+		
+		
 		finish = System.currentTimeMillis();
+		
+		
+		
+		
+		
+		
 		System.out.println("Response time: " + (finish - start) + " ms");
 		CommonPF.quite();
 	}
